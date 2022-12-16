e1cac8c80a1c9221080d20e423a0772e67a919ab
Aleksandr <medovnikaleksander@yandex.ua>
2022-12-16 17:02:16 +0300

    added redux

--- frontend/gui/src/store/actions/auth.js
+++ frontend/gui/src/store/actions/auth.js
@@ -5,3 +5,19 @@ import axios from 'axios';
 export const authStart = () => ({ type: actionTypes.AUTH_START })
 export const authSuccess = token => ({ type: actionTypes.AUTH_SUCCESS, token })
 export const authFail = error => ({ type: actionTypes.AUTH_FAIL, error })
+
+export const authLogin = (username, password) => {
+    return dispatch => {
+        dispatch(authStart())
+        axios.post('http://127.0.0.1:8000/auth/token/login/', {
+            username,
+            password
+        })
+            .then(response => {
+                console.log(response.data)
+            })
+            .catch(err => {
+                dispatch(authFail(err))
+            })
+    }
+}
